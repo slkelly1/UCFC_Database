fb54e14b47cda1a778e8e73ff898d75ebea7ee22
Davide Dal Pos <daveliga@gmail.com>
2025-04-17 13:01:44 -0400

    Home page update

--- pages/home.vue
+++ pages/home.vue
@@ -4,11 +4,15 @@
       <SectionData />
       <SectionAnnouncements />
       <SectionTerms class="drop-shadow" />
+    <PanelDescendants />
     </div>
   </template>
+
+
   <script setup>
   import SectionData from './components/Section/SectionData.vue'
   import SectionLandscape from './components/Section/SectionLandscape.vue'
   import SectionTerms from './components/Section/SectionTerms.vue'
     import SectionAnnouncements from './components/Section/SectionAnnouncements.vue'
+    import PanelDescendants from './PanelDescendants.vue'
   </script>
