891aad5cea043deae046f49f47d2aaf473c472fc
Davide Dal Pos <daveliga@gmail.com>
2025-04-17 12:44:57 -0400

    Home page update

---
+++ pages/TaxonomyPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="taxonomy-page">
+      <h1>Taxonomy Tree</h1>
+  
+      <div v-if="taxonomy">
+        <SectionTree :taxonomy="taxonomy" />
+      </div>
+  
+      <div v-else>
+        Loading...
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue'
+  import SectionTree from '@/components/Section/Tree/SectionTree.vue' // <-- same component as the panel!
+  import TaxonWorks from '@/services/TaxonWorks'
+  
+  const taxonomy = ref(null)
+  
+  onMounted(async () => {
+    const { data } = await TaxonWorks.getTaxonomy(1) // <-- replace 1 with the OTU ID you want as root
+    taxonomy.value = data
+  })
+  </script>
+  
